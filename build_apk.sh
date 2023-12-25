a994aca637f7376d25ed560653602acfb510fe32
maciek.glowka <maciek.glowka@gmail.com>
2023-12-25 21:20:15 +0100

    aab build

--- build_apk.sh
+++ build_apk.sh
@@ -1,4 +1,5 @@
-docker run --rm -v $(pwd):/app -v cargo_index:/usr/local/cargo -v $(pwd)/../rogalik:/rogalik --env-file ./.apk_env -t tower_android
 rm ./output/android/ -rf
 mkdir ./output/android/
-cp ./target/release/apk/tower.apk ./output/android/monk_tower.apk
+docker run --rm -v $(pwd):/app -v cargo_index:/usr/local/cargo -v $(pwd)/../rogalik:/rogalik --env-file ./.apk_env -t tower_android
+
+mv ./output/android/bundle.aab ./output/android/monk_tower.aab
---
+++ docker/android/run.sh
@@ -0,0 +1,25 @@
+cargo apk build --release
+
+set -e
+
+TEMP=$(mktemp -d)
+
+/usr/lib/extra/aapt2/aapt2 convert ../../target/release/apk/tower.apk --output-format proto -o $TEMP/app_proto.apk
+
+cd $TEMP
+
+unzip app_proto.apk
+mkdir manifest
+# mkdir dex
+mv AndroidManifest.xml manifest/
+# mv classes.dex dex/
+rm app_proto.apk
+rm META-INF -rf
+
+zip -r base.zip *
+
+cd -
+
+java -jar /usr/lib/extra/bundletool-all-1.15.6.jar build-bundle --modules=$TEMP/base.zip --output=../../output/android/bundle.aab
+
+jarsigner -keystore $CARGO_APK_RELEASE_KEYSTORE -storepass $CARGO_APK_RELEASE_KEYSTORE_PASSWORD ../../output/android/bundle.aab tower
