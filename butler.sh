eaddb36f2d4156bea29d417fa3e716f85f2bcd83
maciek.glowka <maciek.glowka@gmail.com>
2023-12-28 19:35:00 +0100

    apk channel

--- butler.sh
+++ butler.sh
@@ -1,3 +1,4 @@
 ~/dev/.butler/butler push output/windows/ maciekglowka/monk-tower:windows
 ~/dev/.butler/butler push output/linux/ maciekglowka/monk-tower:linux
 ~/dev/.butler/butler push output/wasm/ maciekglowka/monk-tower:wasm
+~/dev/.butler/butler push output/android/apk maciekglowka/monk-tower:android
